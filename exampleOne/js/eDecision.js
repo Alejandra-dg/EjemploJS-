56c6ad75925451f7513b9bed8f278a7fece0c03b
Alejandra-dg <alejandragonza.07@gmail.com>
2020-09-22 21:18:24 -0500

    update if, case, tipo flecha

--- exampleOne/js/eDecision.js
+++ exampleOne/js/eDecision.js
@@ -52,7 +52,7 @@ function mostrarMensajeDos()
 
     // if con operador logico AND(&&)
 
-    if (edadP <= parametroEdad && estaturaP >= parametroEstatura && pesoP == parametroPeso) {
+    /* if (edadP <= parametroEdad && estaturaP >= parametroEstatura && pesoP == parametroPeso) {
         mensajeDecision = "Apto para realizar el deporte"
     } 
     else 
@@ -68,8 +68,77 @@ function mostrarMensajeDos()
     else 
     {
         mensajeDecision = "No Apto. para realizar el deporte"
-    }
-
+    } */
 
     alert(mensajeDecision)
 }
+
+ function mostrarMensajeTres() 
+{
+    let productName = document.getElementById('productName').value;
+    let price = 0
+
+    const PRODUCT_ONE = 'A'
+    const PRODUCT_TWO = 'B'
+    const PRODUCT_THREE = 'C'
+    const MESSAGE = 'invalidct product name'
+
+    /*  if (productName.toUpperCase() == PRODUCT_ONE ) {
+        price = 2000   
+    } 
+    else 
+    { 
+        if (productName.toUpperCase()  == PRODUCT_TWO) 
+        {
+            price = 2200
+        } 
+        else 
+        {
+           if (productName.toUpperCase()  == PRODUCT_THREE) {
+               price = 1800
+           } else 
+           {
+               alert(MESSAGE.toUpperCase())
+           }
+        }        
+    } alert(price)  */ 
+
+    switch (productName.toUpperCase()) 
+    {
+        case PRODUCT_ONE :
+            price = 20000
+           break;
+        case PRODUCT_TWO :
+            price = 22000
+           break;
+        case PRODUCT_THREE:
+            price = 18000
+           break;
+        default:
+            alert(MESSAGE.toUpperCase())
+            break;
+    }
+    alert(Intl.NumberFormat().format(price))
+}
+
+//function prenderApagar(interruptor) 
+let prenderApagar = interruptor => {
+    let pic 
+
+   /*  if (interruptor == 0) {
+        alert('OF')
+    } else {
+        alert('ON')
+    } */
+
+    let mensaje = (interruptor == 0) ? "OF" : "ON"
+
+    alert(mensaje)
+    
+}
+
+
+
+
+
+
